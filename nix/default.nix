d11e4cb13f25923a572c768c420d8f40cee8a4e1
bcasale <benjamin.casale@proton.me>
2025-09-26 10:10:27 +0200

    chore(nix): wip new flake

--- nix/default.nix
+++ nix/default.nix
@@ -4,7 +4,12 @@ in
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "rcmtool";
   version = manifest.workspace.package.version;
-  cargoLock.lockFile = ../Cargo.lock;
+  cargoLock = {
+     lockFile = ../Cargo.lock;
+     outputHashes = {
+       "quick-xml-0.36.0" = "sha256-1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z";
+     };
+   };
   src = pkgs.lib.cleanSource ../.;
   useNextest = true;
   buildAndTestSubdir ="cmtool";
